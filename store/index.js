c5b36c4b77d06fb2fc12c82b2d371312a5d77f5a
Kévin FURET <kefuret@gmail.com>
2018-08-24 13:47:11 +0200

    :bug: wrong way to define store

--- store/index.js
+++ store/index.js
@@ -1,19 +1,23 @@
 import Vuex from 'vuex'
 
-const store = new Vuex.Store({
-  state: {
-    menu: {}
-  },
-  mutations: {
-    set(state, data){
-      state.menu = data
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      menu: {}
+    },
+    mutations: {
+      set(state, data){
+        state.menu = data
+      }
+    }, 
+    actions: {
+      async nuxtServerInit ({ commit }, { app }) {
+        const response = await app.$axios.get("https://jsonplaceholder.typicode.com/todos")
+        commit('set', response.data)
+        console.log(response.data)
+      }
     }
-  }, 
-  actions: {
-    async nuxtServerInit ({ commit }, { app }) {
-      const data = await app.$axios.get("https://jsonplaceholder.typicode.com/todos")
-      commit('set', response.data)
-      console.log(response.data)
-    }
-  }
-})
+  })
+}
+
+export default createStore
