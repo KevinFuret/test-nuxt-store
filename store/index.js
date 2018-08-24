64605c89b71580e28be82c37e80b3f5879d2f410
Kévin FURET <kefuret@gmail.com>
2018-08-24 14:34:58 +0200

    :bug: Use axios by direct import

--- store/index.js
+++ store/index.js
@@ -1,9 +1,10 @@
 import Vuex from 'vuex'
+import axios from 'axios'
 
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      menu: {}
+      menu: []
     },
     mutations: {
       set(state, data){
@@ -11,10 +12,12 @@ const createStore = () => {
       }
     }, 
     actions: {
-      async nuxtServerInit ({ commit }, { app }) {
-        const response = await app.$axios.get("https://jsonplaceholder.typicode.com/todos")
-        commit('set', response.data)
-        console.log(response.data)
+      async nuxtServerInit ({ commit }) {
+        axios.get('https://jsonplaceholder.typicode.com/todos')
+          .then(function (response) {
+            commit('set', response.data)
+            console.log(response.data);
+          })
       }
     }
   })
